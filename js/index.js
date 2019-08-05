673b5625c7eb658afb77531b1c8896621315aaae
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-08-05 13:20:43 -0700

    Updated the html with the json data

--- js/index.js
+++ js/index.js
@@ -50,10 +50,32 @@ navlinks[3].textContent = "Features";
 navlinks[4].textContent = "About";
 navlinks[5].textContent = "Contact";
 
-const ctastuff = document.getElementsByTagName("h1");
-const maincontentstuff = document.getElementsByTagName("div", "h4", "p");
-const contactstuff = document.getElementsByClassName("contact");
-const bot = document.getElementsByTagName("footer");
+const ctastuff = document.querySelectorAll("h1");
+ctastuff[0].textContent = "DOM is Awesome!";
+
+const fourhead = document.querySelectorAll("h4");
+fourhead[0].textContent = "Features";
+fourhead[1].textContent = "About";
+fourhead[2].textContent = "Services";
+fourhead[3].textContent = "Product";
+fourhead[4].textContent = "Vision";
+
+const content = document.querySelectorAll("p");
+content[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+content[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+content[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+content[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+content[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+const middleImg = document.querySelector(".middle-img");
+middleImg.setAttribute('src','img/mid-page-accent.jpg');
+
+
+fourhead[5].textContent = "Contact";
+content[5].textContent = "123 Way 456 Street Somewhere, USA";
+content[6].textContent = "1 (888) 888-8888";
+content[7].textContent = "sales@greatidea.io";
+content[8].textContent = "Copyright Great Idea! 2018";
 
 const ctaimg = document.querySelector("#cta-img");
 ctaimg.setAttribute('src', 'img/header-img.png');
